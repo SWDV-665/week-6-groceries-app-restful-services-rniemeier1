a37ba76e120de84c272a235110fd486331ceed20
R. Niemeier <48067061+rniemeier@users.noreply.github.com>
2022-11-28 00:57:36 -0600

    Tutorial

--- groceries/src/app/groceries-service.service.ts
+++ groceries/src/app/groceries-service.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import {Observable} from 'rxjs';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import {map, catchError} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 
 @Injectable({
@@ -6,13 +11,41 @@ import { Injectable } from '@angular/core';
 })
 export class GroceriesServiceService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   //List of Groceries
   items = [];
+  dataChanged$: Observable<boolean>;
+  private dataChangeSubject: Subject<boolean>;
+  baseURL = "http://localhost:8080";
 
-  getItems(){
-    return this.items;
+  //getItems(){
+  //  return this.items;
+  //}
+
+  getItems():Observable<object[]>{
+    return <Observable<object[]>> this.http.get(this.baseURL + '/api/groceries').pipe(
+      map(this.extractData),
+      catchError(this.handleError)
+    );
+  }
+
+  private extractData(res: Response){
+    let body = res;
+    return body || {};
+  }
+
+  private handleError(error: Response | any){
+    let errMsg: string;
+    if(error instanceof Response){
+      const err = error || '';
+      errMsg = err.toString() ? error.status + " - " + error.statusText : err.toString();
+    }
+    else{
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
   removeItem(index){
